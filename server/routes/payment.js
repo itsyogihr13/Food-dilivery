3f2315900f0f75c77c9a4b0a7ee518943b7ce4b3
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-13 22:42:03 +0530

    dot.env

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -28,4 +28,25 @@ router.post("/orders", async (req, res) => {
   }
 });
 
+router.post("/verify", async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSign = crypto
+      .createHmac("sha256", process.env.KEY_SECRET)
+      .update(sign.toString())
+      .digest("hex");
+
+    if (razorpay_signature === expectedSign) {
+      return res.status(200).json({ message: "Payment verified successfully" });
+    } else {
+      return res.status(400).json({ message: "Invalid signature sent!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error!" });
+    console.log(error);
+  }
+});
+
 module.exports = router;
