f33e0a9d685f0cbf2a2a5946a1098cfbecd5c102
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-21 13:25:27 +0530

    state

--- src/components/CardsData.js
+++ src/components/CardsData.js
@@ -1,112 +1,139 @@
 const Cardsdata = [
-    {
-        id: 1,
-        rname: "Massala Theoryy",
-        imgdata: "https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp",
-        address: "North Indian, Biryani, Mughlai",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 1175 + order placed from here recently",
-        price: 350,
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-        qnty:0
-    },
-    {
-        id: 2,
-        rname: "Jugaadi Adda",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/19295245/089cbcf1d3307542c72f77272556b28b_o2_featured_v2.jpg?output-format=webp",
-        address: "Street Food",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 2525 + order placed from here recently",
-        price: 25,
-        rating: "3.9",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 3,
-        rname: "La Milano Pizzeria",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/19708611/10f90d4a69678d98662514d173b29665_o2_featured_v2.jpg",
-        address: "Pizza, Fast Food, Pasta",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 650 + order placed from here recently",
-        price: 70,
-        rating: "4.2",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 4,
-        rname: "Momoman",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/113401/59f29399060caefcc575d59dc9402ce8_o2_featured_v2.jpg",
-        address: "Momos",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 300 + order placed from here recently",
-        price: 70,
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 5,
-        rname: "Jassi De Parathe",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110225/3978e28854b7496dbef9496546734811_o2_featured_v2.jpg",
-        address: "North Indian",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: "1050 + order placed from here recently",
-        price: 210,
-        rating: "4.0",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 6,
-        rname: "Anjoy Latenight Meals",
-        imgdata: "https://b.zmtcdn.com/data/pictures/5/113895/3c06f6fbb8f667a2537dfdb6f060dc8b_o2_featured_v2.jpg",
-        address: "Wraps FastFood, Chines",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 1100 + order placed from here recently",
-        price: 100,
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 7,
-        rname: "Hocco Eatery",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110155/811c01a5430d50d3260f77917da99e12_o2_featured_v2.jpg",
-        address: "North Indian, Fast Food",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: "500 + order placed from here recently",
-        price: 300,
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 8,
-        rname: "Chai Wai",
-        imgdata: "https://b.zmtcdn.com/data/pictures/3/18514413/0a17b72e9fec52a3ca736f4c2ea3646f_o2_featured_v2.jpg",
-        address: "Tea, Coffee, Shake, Beverages",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: "500 + order placed from here recently",
-        price: 100,
-        rating: "3.2",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
-    {
-        id: 9,
-        rname: "HL Frankie",
-        imgdata: "https://b.zmtcdn.com/data/pictures/7/19639627/94c0a4cf15c02d3982d154e2c5dd8cbb_o2_featured_v2.jpg",
-        address: "Burger, Sandwich, Fast Food",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: "2525 + order placed from here recently",
-        price: 100,
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
-         qnty:0
-    },
+  {
+    id: 1,
+    rname: "Burger Combo",
+    imgdata:
+      "https://images.unsplash.com/photo-1460306855393-0410f61241c7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8anVuayUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500",
+    address: "North Indian, Biryani, Mughlai",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1175 + order placed from here recently",
+    price: 280,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 2,
+    rname: "Sandwich role",
+    imgdata:
+      "https://images.unsplash.com/photo-1528279027-68f0d7fce9f1?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "Street Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 2525 + order placed from here recently",
+    price: 125,
+    rating: "3.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 3,
+    rname: "Choco lava cake",
+    imgdata:
+      "https://images.unsplash.com/photo-1454255779048-55ecd78837d4?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "Pizza, Fast Food, Pasta",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 650 + order placed from here recently",
+    price: 99,
+    rating: "4.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 4,
+    rname: "Chezze Pizza",
+    imgdata:
+      "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "Momos",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 300 + order placed from here recently",
+    price: 69,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 5,
+    rname: "Ice-cream Shake",
+    imgdata:
+      "https://images.unsplash.com/photo-1481391032119-d89fee407e44?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "North Indian",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "1050 + order placed from here recently",
+    price: 110,
+    rating: "4.0",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 6,
+    rname: "Red chilli Pasta",
+    imgdata:
+      "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "Wraps FastFood, Chines",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1100 + order placed from here recently",
+    price: 149,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 7,
+    rname: "Choco cake",
+    imgdata:
+      "https://images.unsplash.com/photo-1488477304112-4944851de03d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "North Indian, Fast Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + order placed from here recently",
+    price: 300,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 8,
+    rname: "2 min Meggie",
+    imgdata:
+      "https://images.unsplash.com/photo-1526394158538-882527143da6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTd8fGp1bmslMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500",
+    address: "Tea, Coffee, Shake, Beverages",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + order placed from here recently",
+    price: 100,
+    rating: "3.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 9,
+    rname: "HL Frankie",
+    imgdata:
+      "https://b.zmtcdn.com/data/pictures/7/19639627/94c0a4cf15c02d3982d154e2c5dd8cbb_o2_featured_v2.jpg",
+    address: "Burger, Sandwich, Fast Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "2525 + order placed from here recently",
+    price: 100,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
 ];
 
 export default Cardsdata;
